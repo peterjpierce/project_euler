d19e033afcf6a7be6aad7fc29de6f38d0dfa7519
Peter Pierce <peter@truenorth.us>
2016-12-25 10:58:35 -0500

    backfit timing and standard format into 001

--- solutions/001.py
+++ solutions/001.py
@@ -1,5 +1,21 @@
-#!/usr/bin/env python
+"""
+Problem 1
 
-answer = sum([i for i in range(1000) if not (i % 3 and i % 5)])
+If we list all the natural numbers below 10 that are multiples of 3 or 5,
+we get 3, 5, 6 and 9. The sum of these multiples is 23.
 
-print(answer)
+Find the sum of all the multiples of 3 or 5 below 1000.
+"""
+from shared import util
+
+
+def run():
+    start_time = util.now()
+
+    answer = sum([i for i in range(1000) if not (i % 3 and i % 5)])
+    print('answer is: %s' % str(answer))
+    print('elapsed seconds: %f' % (util.now() - start_time).total_seconds())
+
+
+if __name__ == '__main__':
+    run()
