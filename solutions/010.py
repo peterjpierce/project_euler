1d6996b14f548ca1675cc0f8743ca4a587e8e054
Peter Pierce <peter@truenorth.us>
2016-12-25 22:04:14 -0500

    use shared.util.prime() for 010

--- solutions/010.py
+++ solutions/010.py
@@ -1,39 +1,25 @@
 """
 Problem 10
+
 The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+
 Find the sum of all the primes below two million.
 """
-from datetime import datetime as dtime
-
-
-def primes(maximum_value, verbose=True):
-    """Generate a sequence of prime numbers, using Sieve of Eratosthenes."""
-    known_nonprimes = set()
-    range_stop = maximum_value + 1
-
-    for cursor in range(2, range_stop):
-        if cursor not in known_nonprimes:
-            more_nonprimes = range(cursor ** 2, range_stop, cursor)
-            known_nonprimes.update(more_nonprimes)
-
-            if verbose:
-                print('found prime: %12d' % cursor)
-
-            yield cursor
+from shared import util
 
 
 def run():
-    start_time = dtime.now()
+    start_time = util.now()
     total, count = 0, 0
     maximum = 2000000
 
-    for prime in primes(maximum, verbose=True):
+    for prime in util.primes(maximum, verbose=True):
         count += 1
         total += prime
 
     answer = {'sum': total, 'count': count}
     print('answer is: %s' % str(answer))
-    print('elapsed seconds: %f' % (dtime.now() - start_time).total_seconds())
+    print('elapsed seconds: %f' % (util.now() - start_time).total_seconds())
 
 
 if __name__ == '__main__':
